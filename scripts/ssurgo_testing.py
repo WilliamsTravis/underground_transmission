1ee9bb20f028ebd6333c3ed80236c27575daf865
Travis <travis@debian-BULLSEYE-live-builder-AMD64>
2021-12-09 09:26:08 -0700

    Using the rasters now, added nice brockdepmin image for Colorado.

--- scripts/ssurgo_testing.py
+++ scripts/ssurgo_testing.py
@@ -11,7 +11,17 @@ import numba as nb
 import numpy as np
 import rasterio as rio
 
+from numba import float64, int32, int64
 from ssurgo import NATSGO, SSURGO, HOME
+from tqdm import tqdm
+
+
+# @nb.guvectorize([(int32[:], float64[:])], "(m,n),(m,n)")
+def mapit(sarray, mapping):
+    """Mapping values to an array."""
+    for i in range(0, mapping.shape[1] - 1):
+        key, value = mapping[:, i:i+1]
+        sarray[sarray == key] = value
 
 
 def to_raster(rpath, table, variable, dst):
@@ -26,14 +36,21 @@ def to_raster(rpath, table, variable, dst):
     table = table[["mukey", variable]].drop_duplicates()
     table = table[~np.isnan(table[variable])]
 
-    # Create a nice dictionary from the table variable
-    mapping = dict(zip(table["mukey"], table[variable]))
+    # Create a 2D vector as a stand in for a dictionary ("scalars" required)
+    mapping = np.vstack([table["mukey"], table[variable]])
+    mapdict = dict(zip(table["mukey"], table[variable]))
+
+    # Assign values one section at a time
     arrays = np.array_split(array, 100)
-    out = []
-    for i, array in enumerate(arrays):
-        if i == 50:
-            break
-        array2 = np.vectorize(mapping.get)(array)
+    narrays = []
+    for tarray in arrays:
+        mapit(tarray, mapping)
+        narrays.append(tarray)
+
+
+
+
+
 
 
 if __name__ == "__main__":
