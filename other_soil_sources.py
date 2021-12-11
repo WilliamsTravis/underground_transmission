1915178723bb78788250c8b52d3cfc4a968843c6
williamstravis <travis.williams@nrel.gov>
2021-12-11 13:05:18 -0700

    Added two new sources for soil information.

---
+++ other_soil_sources.py
@@ -0,0 +1,23 @@
+"""Other Soil data sets.
+
+Created on Sat Dec 11 12:57:03 2021
+
+@author: twillia2
+"""
+import requests
+
+
+GLOBAL_DTB = ("http://globalchange.bnu.edu.cn/download/data/DTB/"
+              "BDTICM_M_10km_ll.zip")
+GLOBAL_DTB_C = ("http://globalchange.bnu.edu.cn/download/data/DTB/"
+                "BDRICM_M_10km_ll.zip")
+GLOBAL_DTB_CITE = ("Shangguan, W., Hengl, T., de Jesus, J.M., Yuan, H. and "
+                   "Dai, Y., 2017. Mapping the global depth to bedrock for "
+                   "land surface modeling. Journal of Advances in Modeling "
+                   "Earth Systems: Accepted.")
+GLOBAL_ST = ("https://daac.ornl.gov/cgi-bin/dsviewer.pl?ds_id=1304")
+GLOBAL_ST_CITE = ("Pelletier, J.D., P.D. Broxton, P. Hazenberg, X. Zeng, P.A. "
+                  "Troch, G. Niu, Z.C. Williams, M.A. Brunke, and D. Gochis. "
+                  "2016. Global 1-km Gridded Thickness of Soil, Regolith, and "
+                  "Sedimentary Deposit Layers. ORNL DAAC, Oak Ridge, "
+                  "Tennessee, USA. https://doi.org/10.3334/ORNLDAAC/1304")
